5220a76273270862213de23e800b9db5718d0cbf
Abhay Kadam <abhaykadam88@gmail.com>
2014-04-18 13:44:44 +0530

    Update test_active_ftp.rb

--- test/test_active_ftp.rb
+++ test/test_active_ftp.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require 'active_ftp'
 
-class HolaTest < Test::Unit::TestCase
+class ActiveFtpTest < Test::Unit::TestCase
   def test_active_ftp_instance
     assert_not_nil ActiveFtp.new
   end
