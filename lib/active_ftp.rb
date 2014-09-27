84027c15190e4c39ed3c84b2b4717074749e8386
Abhay Jayant Kadam <abhaykadam88@gmail.com>
2014-09-27 20:25:30 +0530

    Adds logger to the gem

--- lib/active_ftp.rb
+++ lib/active_ftp.rb
@@ -1,11 +1,17 @@
 require 'net/ftp'
 require 'net/ftp/list'
 
+require 'logger'
+$log = Logger.new('active_ftp.log', 'monthly')
+
 class ActiveFtp < Net::FTP
 
   def rm_r(path)
+    $log.debug {'In rm_r()'}
     dir_oper = Proc.new {|abs_dir| rmdir abs_dir}
     file_oper = Proc.new {|abs_file| delete abs_file}
+
+    $log.debug {"Starting removing tree at #{path}"}
     depth_first(path, dir_oper, file_oper)
 
     true
@@ -20,9 +26,13 @@ private
       entry_path = File.join(path, entry.basename)
 
       if entry.dir?
+        $log.debug {"In #{entry.basename} directory"}
+
         depth_first(entry_path, dir_oper, file_oper)
         dir_oper.call entry_path
       elsif entry.file?
+        $log.debug {"At #{entry.basename} file"}
+
         file_oper.call entry_path
       end
     end
